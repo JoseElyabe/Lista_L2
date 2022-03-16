5ac504e4f82443864f4888e6b9f7b7c0189931f9
JoseElyabe <96116598+JoseElyabe@users.noreply.github.com>
2022-03-16 15:01:23 -0300

    Correção de erro.

--- Q7.c
+++ Q7.c
@@ -3,30 +3,31 @@
 
 void mA(int);
 
-int nq = 1, vS;
 const int vt = 6;
+int vS = 0;
+float nq = 0;
 
 int numeroS[vt];
 int main () {
 
-    for(nq ; nq <= 6; nq ++) {
-
+    while(nq < 6) {
+        
+        nq = ++nq;
         printf("Digite um numero \n");
         scanf("%d",&numeroS[vS]);
         
         vS = ++vS;
-        printf("%d",nq);
+        printf("\n%g",nq);
     }
 
     mA(numeroS[vS]);
     
+    
     return 0;
 }
-
 void mA() {
 
     float mediaA;
-    mediaA = (numeroS[0] + numeroS[1] + numeroS[2] + numeroS[3] + numeroS[4] + numeroS[5]) /nq;
-    printf("A media aritimetica dos numeros digitados é %f", mediaA);
-
+    mediaA =(numeroS[0] + numeroS[1] + numeroS[2] + numeroS[3] + numeroS[4] + numeroS[5]) /nq;
+    printf("A media aritimetica dos numeros digitados é %g", mediaA);
 }
